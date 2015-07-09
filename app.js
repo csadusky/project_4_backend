d9e25b677df8874fd75ccef501ffa41b2f832156
courtneysadusky <courtneysadusky@gmail.com>
2015-07-09 10:21:51 -0400

    set up config

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
+var config = require('./config');
+var mongoose = require('mongoose');
+var logger= require('morgan');
+var express_session = require('express-session');
+
+
+//connecting app to mongo database
+mongoose.connect(config.mongo.dbUrl);
+
+app.use(logger());
+//Use for authentication
+// app.use(express_session({secret:"its a secret"}));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
--- config/config.development.js
+++ config/config.development.js
@@ -1,23 +1,15 @@
 var config = require('./config.global.js');
 
-// do we have a secret keys file?
 
-try {
-  var jsonKeys = require('./secrets.json');
-} catch (e) {
-  if (e.code !== 'MODULE_NOT_FOUND') {
-    throw e;
-  }
-}
+config.env = 'development';
+config.hostname = 'localhost';
+config.serverPort = 5000;
 
-config.secretNames.forEach(function(name) {
-  if (process.env[name] !== undefined) {
-    config.secrets[name] = process.env[name];
-  } else if (jsonKeys[name] !== undefined) {
-    config.secrets[name] = jsonKeys[name];
-  } else {
-    console.warn('No value found for secret %s', name);
-  }
-});
+
+config.authStrategy = 'local';
+
+config.mongo = {};
+config.mongo.dbUrl = 'mongodb://localhost/beaches';
+
 
 module.exports = config;
