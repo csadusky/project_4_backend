5589de155ace8a4aade425f87d6c0144bb535462
courtneysadusky <courtneysadusky@gmail.com>
2015-07-13 14:26:38 -0400

    testing instagram auth

--- app.js
+++ app.js
@@ -15,40 +15,40 @@ app.use(logger());
 
 //Use for authentication
 // app.use(express_session({secret:"its a secret"}));
-var api = require('instagram-node').instagram();
+// var api = require('instagram-node').instagram();
 
-// app.configure(function() {
+// // app.configure(function() {
+// //   client_id: process.env.YOUR_CLIENT_ID,
+// //   client_secret: process.env.YOUR_CLIENT_SECRET
+// // });
+
+// api.use({
 //   client_id: process.env.YOUR_CLIENT_ID,
 //   client_secret: process.env.YOUR_CLIENT_SECRET
 // });
 
-api.use({
-  client_id: process.env.YOUR_CLIENT_ID,
-  client_secret: process.env.YOUR_CLIENT_SECRET
-});
+// var redirect_uri = 'https://fast-brook-4146.herokuapp.com/auth';
 
-var redirect_uri = 'https://fast-brook-4146.herokuapp.com/auth';
+// authorize_user = function(req, res) {
+//   res.redirect(api.get_authorization_url(redirect_uri));
+// };
 
-authorize_user = function(req, res) {
-  res.redirect(api.get_authorization_url(redirect_uri));
-};
+// handleauth = function(req, res) {
+//   api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+//     if (err) {
+//       console.log(err.body);
+//       res.send("Didn't work");
+//     } else {
+//       console.log('Yay! Access token is ' + result.access_token);
+//       res.send('You made it!!');
+//     }
+//   });
+// };
 
-handleauth = function(req, res) {
-  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
-    if (err) {
-      console.log(err.body);
-      res.send("Didn't work");
-    } else {
-      console.log('Yay! Access token is ' + result.access_token);
-      res.send('You made it!!');
-    }
-  });
-};
-
-// This is where you would initially send users to authorize
-app.get('/', authorize_user);
-// This is your redirect URI
-app.get('/auth', handleauth);
+// // This is where you would initially send users to authorize
+// app.get('/', authorize_user);
+// // This is your redirect URI
+// app.get('/auth', handleauth);
 
 
 
