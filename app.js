16f7a365f0eb9e2ea4177d39feb8833f026c5bc5
courtneysadusky <courtneysadusky@gmail.com>
2015-07-16 14:59:53 -0400

    changes app js to try and get updated pic from insta again

--- app.js
+++ app.js
@@ -92,10 +92,10 @@ app.get('/beaches/:name', function(req, res){
     if(!beach.length) {
       return res.sendStatus(404);
     }
-    if(beach[0].thumbnail) {
+    if(!beach[0].thumbnail) {
       beach[0].getPictures(api).then(function(media) {
         // insert into database
-        var thumb = media[0].images.thumbnail.url;
+        var thumb = media.images.thumbnail.url;
         beach[0].thumbnail = thumb;
         beach[0].save(function(err) {
           if(err) {
