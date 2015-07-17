2a9f002bc7f77cc3625be62954b6cb928020573e
courtneysadusky <courtneysadusky@gmail.com>
2015-07-16 20:07:35 -0400

    trys to change image one more time

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.get('/beaches/:name', function(req, res){
     }
       beach[0].getPictures(api).then(function(media) {
         // insert into database
-        var thumb = media[0].images.thumbnail.url;
+        var thumb = media[1].images.thumbnail.url;
         beach[0].thumbnail = thumb;
         //for (thumb = 0; thumb < 10; thumb++){
         beach[0].save(function(err) {
