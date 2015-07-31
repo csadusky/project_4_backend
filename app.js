28db9cc7191a91a3c3f6db8e7a795ba58f9d2e19
courtneysadusky <courtneysadusky@gmail.com>
2015-07-31 15:10:26 -0400

    put in debigger

--- app.js
+++ app.js
@@ -94,9 +94,9 @@ app.get('/beaches/:name', function(req, res){
     }
       beach[0].getPictures(api).then(function(media) {
         // insert into database
+        debugger
         var thumb = media[1].images.thumbnail.url;
         beach[0].thumbnail = thumb;
-        //for (thumb = 0; thumb < 10; thumb++){
         beach[0].save(function(err) {
           if(err) {
             return res.sendStatus(500);
